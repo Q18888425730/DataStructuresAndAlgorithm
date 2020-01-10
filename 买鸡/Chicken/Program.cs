3604459817d536399206d1d86255b4ce95d33b8e
Q18888425730 <58315066+Q18888425730@users.noreply.github.com>
2020-01-10 10:18:32 +0800

    Add files via upload

---
+++ 买鸡/Chicken/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Chicken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //公鸡的数量
+            int cockCount = 0;
+            //母鸡的数量
+            float henCount = 0;
+            //小鸡的数量
+            float smallCount = 0;
+            for (cockCount = 0; cockCount < 100; cockCount++)
+            {
+                henCount = 25 - 7f / 4f * cockCount;
+                smallCount = 75 + 3f / 4f * cockCount;
+                if (henCount > 0 && smallCount > 0)
+                {
+                    if (((int)henCount == henCount) && ((int)smallCount == smallCount))
+                    {
+                        Console.WriteLine("公鸡数目有:{0},母鸡数目有:{1},小鸡数目有:{2}", cockCount, henCount, smallCount);
+                    }
+
+                }
+
+            }
+        }
+    }
+}
